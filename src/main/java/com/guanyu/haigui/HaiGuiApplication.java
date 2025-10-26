d9e97c06d932ebe24f9fd48dc776afb6f0a93a5d
qingyou <486683998@qq.com>
2025-10-26 11:53:47 +0800

    游戏大厅

--- src/main/java/com/guanyu/haigui/HaiGuiApplication.java
+++ src/main/java/com/guanyu/haigui/HaiGuiApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoCo
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@EnableMethodSecurity()
+//RedisHash注解会将实体类映射为 Redis 的 Hash 类型，但是当前并不需要 Redis 存储，因此先排除 Redis 配置
 @SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
+@EnableMethodSecurity()
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @MapperScan(basePackages = "com.guanyu.haigui.mapper")
 public class HaiGuiApplication {
